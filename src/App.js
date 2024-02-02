447edebd8197a12341ce386e3a5889022e16c231
Varunbingi <bingivarun27@gmail.com>
2024-02-02 15:42:00 +0530

    pw skills react assignment week1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Person from "./Person";
+import Button from "./Button";
+import Header from "./Header";
+import List from "./List";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="welcome to india" />
+      <Person name="Varun" age="18" />
+      <Button text="click" onClick={() => console.log("clicked")} />
+      <List items={["car", "bike", "apple", "jet", "cycle"]} />
     </div>
   );
 }
---
+++ src/List.js
@@ -0,0 +1,12 @@
+const List = ({ items }) => {
+  return (
+    <>
+      <ul>
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+export default List;
